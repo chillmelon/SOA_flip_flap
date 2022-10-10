c754c0cca1048ab3b7139fa160dbf7292726d718
chillmelon <chiyuh1996@gmail.com>
2022-10-10 19:37:04 +0800

    add: working version

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,38 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  attr_accessor :data
+
+  @data = []
+
+  def take_tsv(tsv)
+    @data = []
+    tsv_data = tsv.split("\n")
+    keys = tsv_data[0].split("\t")
+    tsv_data.shift
+    tsv_data.each do |line|
+      hash = {}
+      item = line.split("\t")
+      keys.each_index { |idx| hash[keys[idx]] = item[idx] }
+      @data.push(hash)
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    result = String.new
+    headers = @data.first.keys.join("\t")
+    result << "#{headers}\n"
+    @data.each do |item|
+      line = String.new
+      line << item.values.join("\t")
+      result << "#{line}\n"
+    end
+    result
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,19 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  attr_accessor :data
+
+  @data = []
+
+  def take_yaml(yaml)
+    @data = []
+    @data = YAML.safe_load(yaml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
+end
